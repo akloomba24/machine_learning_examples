080256c9d3ed6380530df70f0c3d754f0285725b
akloomba24 <ashish.k.loomba@ntnu.no>
2024-02-22 13:10:24 +0100

    Update lecture10_11_EpsilonGreedy.py
    Added one of the decaying functions in the epsilon_greedy code

--- homework/lecture10_11_EpsilonGreedy.py
+++ homework/lecture10_11_EpsilonGreedy.py
@@ -14,6 +14,8 @@ import numpy as np
 
 NUM_TRIALS = 10000
 EPS = 0.1
+EPS_MIN = EPS/1000
+EPS_INIT = EPS
 BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
 
 # Class BanditArm
@@ -40,7 +42,8 @@ def choose_random_argmax(a):
   idx = np.argwhere(np.amax(a) == a).flatten()
   return np.random.choice(idx)
 
-def experiment():
+def experiment(option):
+    global EPS
     bandits = [BanditArm(p) for p in BANDIT_PROBABILITIES]
 
     rewards = np.zeros(NUM_TRIALS) # to store all the rewards for the trials
@@ -53,14 +56,34 @@ def experiment():
     print("optimal j:", optimal_j)
     for i in range(NUM_TRIALS):
 
-        # use epsilon-greedy to select the next bandit
-        if np.random.random() < EPS:
-            num_times_explored += 1
-            j = np.random.randint(len(bandits)) # Randomly select amongst the bandits
-        else:
+        if option == 'Greedy':
+            # use greedy to select the next bandit
             num_times_exploited += 1
             #  if there are multiple maximum values in the array, randomly select one of their indices
             j =  choose_random_argmax([b.p_estimate for b in bandits])
+
+        elif option == 'EpsilonGreedy':
+            # use  epsilon-greedy to select the next bandit
+            if np.random.random() < EPS:
+                num_times_explored += 1
+                j = np.random.randint(len(bandits)) # Randomly select amongst the bandits
+            else:
+                num_times_exploited += 1
+                #  if there are multiple maximum values in the array, randomly select one of their indices
+                j =  choose_random_argmax([b.p_estimate for b in bandits])
+
+        elif option == 'DecayingEG':
+            # use decaying epsilon-greedy to select the next bandit
+
+            EPS = max(EPS_INIT - 0.00001 * NUM_TRIALS, EPS_MIN)
+            if np.random.random() < EPS:
+                num_times_explored += 1
+                j = np.random.randint(len(bandits)) # Randomly select amongst the bandits
+            else:
+                num_times_exploited += 1
+                #  if there are multiple maximum values in the array, randomly select one of their indices
+                j =  choose_random_argmax([b.p_estimate for b in bandits])
+
 
         if j == optimal_j:
             num_optimal += 1
@@ -94,4 +117,5 @@ def experiment():
 
 
 if __name__ == "__main__":
-    experiment()
+    options = ['Greedy','EpsilonGreedy', 'DecayingEG']
+    experiment(options[1])
